a82447a58c025b8195db29aeb27ff8d27c0e397f
Mirvan1 <mirvan.sadiqli@gmail.com>
2024-02-21 02:02:22 +0300

    examples

--- Convertor.ConsoleExamples/Program.cs
+++ Convertor.ConsoleExamples/Program.cs
@@ -3,21 +3,19 @@ using Convertor.ConsoleExamples;
 using System.Reflection;
 
 
-Assembly assembly = typeof(ExampleDtos).Assembly;
+//Assembly assembly = typeof(ExampleDtos).Assembly;
 
-byte[] ex1 = Convertors.SqlToCSharp("Data Source=DESKTOP-HGKR5P9\\MSSQLSERVERDB;Initial Catalog=ExampleDB;Integrated Security=True;Encrypt=false;", true, "sysdiagrams");
-byte[] ex2 = Convertors.SqlToTypescript("Data Source=DESKTOP-HGKR5P9\\MSSQLSERVERDB;Initial Catalog=ExampleDB;Integrated Security=True;Encrypt=false;", true, "sysdiagrams");
+//byte[] ex1 = Convertors.SqlToCSharp("connection-string", true, "except-tables-name-optional");
+//byte[] ex2 = Convertors.SqlToTypescript("connection-string", true, "except-your-tables-name-optional");
 
-Convertors.SqlToTypescript(@"C:/Users/Projects/Convertor/Convertor/Convertor.ConsoleExamples//", "exampleTs", "Data Source=DESKTOP-HGKR5P9\\MSSQLSERVERDB;Initial Catalog=ExampleDB;Integrated Security=True;Encrypt=false;", true, "sysdiagrams");
-Convertors.SqlToCSharp(@"C:/Users/Projects/Convertor/Convertor/Convertor.ConsoleExamples/", "exampleCs", "Data Source=DESKTOP-HGKR5P9\\MSSQLSERVERDB;Initial Catalog=ExampleDB;Integrated Security=True;Encrypt=false;", true, "sysdiagrams");
+//Convertors.SqlToTypescript("path-to-save", "filename.ts", "connection-string", true, "except-tables-name-optional");
+//Convertors.SqlToCSharp("path-to-save", "filename.cs", "connection-string", true, "except-tables-name-optional");
 
 
-string str1 = Convertors.SqlToCSharpStr("Data Source=DESKTOP-HGKR5P9\\MSSQLSERVERDB;Initial Catalog=ExampleDB;Integrated Security=True;Encrypt=false;", true, "sysdiagrams");
-string str2 = Convertors.SqlToTypescriptStr("Data Source=DESKTOP-HGKR5P9\\MSSQLSERVERDB;Initial Catalog=ExampleDB;Integrated Security=True;Encrypt=false;", true, "sysdiagrams");
-Console.WriteLine(str1);
-Console.WriteLine(str2);
+//string str1 = Convertors.SqlToCSharpStr("connection-string", true, "except-tables-name-optional");
+//string str2 = Convertors.SqlToTypescriptStr("connection-string", true, "except-tables-name-optional");
 
-string str3 = Convertors.CsharpToTypescriptStr(assembly);
-byte[] ex3 = Convertors.CSharpToTypescript(assembly);
-Convertors.CSharpToTypescript("C:\\Users\\Lenovo\\source\\repos\\Convertor\\Convertor.ConsoleTest\\", "ts2example", assembly);
+//string str3 = Convertors.CsharpToTypescriptStr(assembly);
+//byte[] ex3 = Convertors.CSharpToTypescript(assembly);
+//Convertors.CSharpToTypescript("path-to-save", "filename.ts", assembly);
  
